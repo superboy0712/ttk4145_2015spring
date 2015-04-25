8169adef001833bf9e42d7e91abddd4e67b9d55d
Yulong Bai <yulongb@stud.ntnu.no>
2015-04-25 14:25:42 +0200

    null

--- system/elevator_model/elevator_model_data_structure.c
+++ system/elevator_model/elevator_model_data_structure.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/time.h>
 #include "../drivers/elev.h"
 #include "../drivers/io.h"
 #include "elevator_model_data_structure.h"
