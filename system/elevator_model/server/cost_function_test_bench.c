664f856b8936f3ada6a89df0a138b97c556ba080
Yulong Bai <yulongb@stud.ntnu.no>
2015-04-25 18:11:28 +0200

    new version cost function. fully tested!

--- system/elevator_model/server/cost_function_test_bench.c
+++ system/elevator_model/server/cost_function_test_bench.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <assert.h>
 #define N_CLIENT 20
 
 struct cost_param_t {
@@ -102,6 +104,69 @@ int cost_function(struct cost_param_t cost_values, int temp_order_floor,
 	//printf(" last dir %c \n", cost_values.direction[the_opt_idx]);
 	return cost_values.index[the_opt_idx];
 }
+#define N_FLOORS 4
+int cost_function_v2(struct cost_param_t cost_values, int order_floor,
+		char temp_order_dircetion) {
+	int the_opt_idx = 0;
+	float temp_order_floor = (float)order_floor;
+	double cost_array[N_CLIENT];
+	int dir = (temp_order_dircetion == 'U')? 1 : -1;
+	/* calculate cost of each connected node */
+	for (int i = 0; i < cost_values.max_connected_nodes; i++) {
+		/* basis cost */
+		cost_array[i] = fabs(temp_order_floor - cost_values.floor_position[i]);
+
+		/* if the cage is moving and moving away from order floor, then add extra cost */
+		if(cost_values.moving_vector[i] != 0){
+			if(cost_values.moving_vector[i]*(temp_order_floor - cost_values.floor_position[i]) <= 0){
+				/* order vector and current moving vector are in opposite direction */
+
+				if(cost_values.moving_vector[i]*dir < 0){
+					/* calling in different direction */
+					if(dir > 0){
+						assert(cost_values.moving_vector[i]<0);
+						cost_array[i] = cost_values.floor_position[i] + temp_order_floor;
+					}else{
+						assert(cost_values.moving_vector[i]>0);
+						cost_array[i] = 2*(N_FLOORS - 1) - cost_values.floor_position[i] - temp_order_floor;
+					}
+				}else{
+					assert(cost_values.moving_vector[i]*dir > 0);
+					cost_array[i] = 2*(N_FLOORS - 1) - fabs(temp_order_floor - cost_values.floor_position[i]);
+				}
+			}
+		}
+
+		/* if exception,  add a large cost */
+		if(cost_values.stop[i] == 1 || cost_values.obstrukt[i] == 1) {
+			cost_array[i] += 1000;/* give the exceptional ones a  large difference */
+		}
+
+	}
+
+    /* find the first minumum one, and return its index in the connected array */
+	float minimum = cost_array[0];
+
+	for(int i = 0; i < cost_values.max_connected_nodes; i++) {
+		if (cost_array[i] < minimum) {
+			minimum = cost_array[i];
+			the_opt_idx = i;
+		}
+	}
+	/* print */
+	printf("The cost_array: ");
+	for(int i = 0; i < cost_values.max_connected_nodes; i++){
+		printf(" {[%d]%4.1lf},", cost_values.index[i], cost_array[i]);
+	}
+	puts("");
+
+	printf("Order is %d_%c, the minimum cost is {%4.1lf from index [%d]}\n",
+			order_floor, temp_order_dircetion, cost_array[the_opt_idx], cost_values.index[the_opt_idx]);
+	if(the_opt_idx == 0)/*< myself */
+		return 0;
+
+	return cost_values.index[the_opt_idx];
+}
 struct cost_param_t t1 = {
 		.floor = { 0, 1, 2, 3, 0, 1, 2, 3 },
 		.direction = { 'U','U','U','U','D','D','D','D'},
@@ -115,6 +180,7 @@ struct cost_param_t t2 = {
 		.direction = { 'U','U','U','U','D','D','D','D'},
 		.stop = { 1, 1, 1, 1, 0, 0, 0, 0 },
 		.obstrukt = {0},
+		.floor_position = { 0, 1, 2, 3, 0, 1, 2, 3 },
 		.max_connected_nodes = 8,/**??? whether it is index or number of nodes???*/
 		.index = { 10, 11, 12, 13, 14, 15, 16, 17 }
 };
@@ -133,6 +199,7 @@ struct cost_param_t t4 = {
 		.direction = { 'U','U','U','U','D','D','D','D','U','U','U','U','D','D','D','D'},
 		.stop = { 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1 },
 		.obstrukt = {0},
+		.floor_position = { 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 },
 		.max_connected_nodes = 16,/**??? whether it is index or number of nodes???*/
 		.index = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 }
 };
@@ -144,6 +211,30 @@ struct cost_param_t t5 = {
 		.max_connected_nodes = 2,/**??? whether it is index or number of nodes???*/
 		.index = { 10, 11 }
 };
+
+struct cost_param_t t6 = {
+		.floor = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		.direction = { 'U','U','U','U','D','D','D','D','U','U','U','U','D','D','D','D'},
+		.stop = { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+		.obstrukt = {0},
+		.floor_position = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		.max_connected_nodes = 16,/**??? whether it is index or number of nodes???*/
+		.index = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 },
+		.moving_vector = {0, 1, 2, 3, 0, 1, 2, 3, 0, -1, -2, -3, 0, -1, -2, -3}
+};
+/**
+ * moving away test
+ */
+struct cost_param_t t7 = {
+		.floor = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		.direction = { 'U','U','U','U','D','D','D','D','U','U','U','U','D','D','D','D'},
+		.stop = { 0 },
+		.obstrukt = {0},
+		.floor_position = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		.max_connected_nodes = 16,/**??? whether it is index or number of nodes???*/
+		.index = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 },
+		.moving_vector = {0, 1, 2, 3, 0, 1, 2, 3, 0, -1, -2, -3, 0, -1, -2, -3}
+};
 void main(void){
 	char dir = 'U';
 	int idx;
@@ -151,15 +242,37 @@ void main(void){
 //		idx = cost_function(t1, i, dir );
 //		printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
 //	}
-	dir = 'U';
-	for (int i = 0; i < 4; ++i) {
-		idx = cost_function(t5, i, dir );
-		printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
-	}
+//	dir = 'U';
+//	for (int i = 0; i < 4; ++i) {
+//		idx = cost_function_v2(t6, i, dir );
+//		printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
+//	}
 //	dir = 'D';
 //	for (int i = 0; i < 4; ++i) {
 //		idx = cost_function(t4, i, dir );
 //		printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
 //	}
+	int i = 2;
+//	dir = 'U';
+//	idx = cost_function_v2(t7, i, dir );
+//	printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
+//
+//	 i = 2;
+//	dir = 'D';
+//	idx = cost_function_v2(t7, i, dir );
+//	printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
 
+	 i = 1;
+	dir = 'D';
+	idx = cost_function_v2(t7, i, dir );
+	printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
+
+//	 i = 0;
+//	dir = 'D';
+//	idx = cost_function_v2(t7, i, dir );
+//	printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
+//	 i = 0;
+//	dir = 'U';
+//	idx = cost_function_v2(t7, i, dir );
+//	printf("floor %d, dir %c, optimal idx %d\n", i, dir, idx);
 }
